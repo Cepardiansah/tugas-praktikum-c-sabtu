1b456fd8b49e9a52da75a4033982b8129386e34d
Cepardiansah <cepardiansah404@gmail.com>
2024-09-26 11:11:37 +0700

    Add files via upload

---
+++ tugas 1/tugas mandiri 1 cpp.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("NAMA : CEP ARDIANSAH\nJURUSAN : S1 TEKNIK INFORMATIKA\nNIM : 2441056");
+	return 0;
+}
---
+++ tugas 1/tugas mandiri 2 cpp.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int panjang = 10;
+	int lebar = 7;
+	int luas;
+	
+	luas = panjang * lebar;
+	
+	printf("Jadi, luas persegi panjang adalah: %d cm ", luas); 
+	return 0;
+}
